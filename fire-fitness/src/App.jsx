9807f847b5049ec87ce8063f5663bc9271e2677c
Trenton Robinson <graphicsbyharlem@gmail.com>
2025-10-22 04:29:20 +0000

    React-Start commit message

--- fire-fitness/src/App.jsx
+++ fire-fitness/src/App.jsx
@@ -1,11 +1,15 @@
-import Home from './pages/Home'
+import './App.css'
 
 function App() {
 
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      <div className="min-h-screen min-w-screen flex flex-col justify-center align-items-center">
+        <section className="mx-0 my-w-auto flex h-50 w-50 items-center justify-center bg-gray-100">
+          <h1 className="text-red-500">Vite + React</h1>
+        </section>
+      </div>
+    </>
   )
 }
 
